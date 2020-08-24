4a95bdc9cbce299da1675e3e8ced1f24649751ef
s1Sharp <maksim.carkov.201300@gmail.com>
2020-08-24 22:20:45 +0300

    solve_5.py

---
+++ solve_5.py
@@ -0,0 +1,23 @@
+def SynchronizingTables( N, ids, salary):
+    current_id = [0]*N
+    result = [0]*N
+    ids_local_min = max(ids)
+    ids_prev_local_min = 0
+    for i in range(N):
+        for k in range(N):
+            if ids[k] < ids_local_min and ids[k] > ids_prev_local_min:
+                ids_local_min = ids[k]
+                current_id[i] = k
+        ids_prev_local_min = ids_local_min
+        ids_local_min = max(ids) + 1
+    for i in range(N-1):
+        for j in range(N-i-1):
+            if salary[j] > salary[j+1]:
+                salary[j], salary[j+1] = salary[j+1], salary[j]
+    for i in range(N):
+        result[current_id[i]] = salary [i]
+    return result
+
+ids = [50,1,1024,12,13,3]
+salary = [20000, 100000, 90000,10,20,200000]
+print(SynchronizingTables(6,ids,salary))
